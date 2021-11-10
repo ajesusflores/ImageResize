e51ea65430a38ed8ce9d293af702968558470400
Alfonso Jesus Flores <alfonso.flores@craneww.com>
2021-11-10 15:11:46 -0600

    Added usage of alternative resize algorithm

--- ImageResize/ImageProcessor.cs
+++ ImageResize/ImageProcessor.cs
@@ -20,7 +20,8 @@ namespace ImageResize
             int height = sharpImage.Height / 10;
 
             using var stream = new MemoryStream();
-            sharpImage.Mutate(x => x.Resize(width, height, KnownResamplers.Bicubic));
+            sharpImage.Mutate(x => x.Resize(width, height, KnownResamplers.Bicubic)); // Bicubic is the default algorithm
+            //sharpImage.Mutate(x => x.Resize(width, height, KnownResamplers.NearestNeighbor)); // NearestNeighbor is the fastest algorithm, but offers low quality
 
             sharpImage.Save(stream, format);
             return new ImageResizeData
