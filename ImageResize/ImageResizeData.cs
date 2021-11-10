cc49301fdefbafa0f8c5b584dabf39a267550cd8
Alfonso Jesus Flores <alfonso.flores@craneww.com>
2021-11-10 11:47:29 -0600

    Initial solution

---
+++ ImageResize/ImageResizeData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageResize
+{
+    public class ImageResizeData
+    {
+        public double ElapsedMilliseconds { get; set; }
+        public File Image { get; set; }
+    }
+}
